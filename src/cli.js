5962dbf8952f2468f2a35c3b5f8f8ebc1043e89a
sadyvakasov <1oky.clo@gmail.com>
2022-03-10 17:11:57 +0300

    add greetings

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,4 @@
-import readlineSync from 'readline-sync';
-const greetings = () => {
-    const userName = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${userName}!`);
-}
+const greetings = (userName) => {
+  console.log(`Hello, ${userName}!`);
+};
 export default greetings;
