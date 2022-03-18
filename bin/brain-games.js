0904a3eef6ad815dc35a47e0b312e8c4b2e4dd96
sadyvakasov <1oky.clo@gmail.com>
2022-03-18 16:23:48 +0300

    add brain progression games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -4,6 +4,7 @@ import greetings from '../src/cli.js';
 import { questionGenerate } from './brain-even.js';
 import calculateGame from './brain-calc.js';
 import gcd from './brain-gcd.js';
+import brainProgression from './brain-progression.js';
 
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name?');
@@ -23,6 +24,7 @@ for (let i = 0; i < 3; i += 1) {
   }
   console.log(`Congratulations, ${userName}!`);
 }
+
 console.log('What is the result of the expression?');
 
 for (let i = 0; i < 3; i += 1) {
@@ -41,7 +43,34 @@ for (let i = 0; i < 3; i += 1) {
 
 console.log('Find the greatest common divisor of given numbers.');
 for (let i = 0; i < 3; i += 1) {
-  const greatestComDiv = gcd();
-  const twoNumbers = [greatestComDiv[0], greatestComDiv[1]];
-  console.log(`Question: ${twoNumbers}`);
+  const randomNum1 = Math.floor(Math.random() * 50);
+  const randomNum2 = Math.floor(Math.random() * 50);
+  console.log(`Question: ${randomNum1} ${randomNum2}`);
+  const gcdAnswer = readlineSync.question('Your answer:');
+  if (gcdAnswer === String(gcd(randomNum1, randomNum2))) {
+    console.log('Correct!');
+  } else {
+    console.log(`${gcdAnswer} is wrong answer ;(. Correct answer was ${gcd(randomNum1, randomNum2)}!`);
+    console.log(`Let's try again, ${userName}`);
+    break;
+  }
+  console.log(`Congratulations, ${userName}!`);
+}
+
+console.log('What number is missing in the progression?');
+for (let i = 0; i < 3; i += 1) {
+  const indexOfHidenNum = Math.floor(Math.random() * 8);
+  const progress = brainProgression();
+  const hidenNum = progress[indexOfHidenNum];
+  progress[indexOfHidenNum] = '..';
+  console.log(`Question: ${progress}`);
+  const progressionAnswer = readlineSync.question('Your answer:');
+  if (String(hidenNum) === progressionAnswer) {
+    console.log('Correct!');
+  } else {
+    console.log(`${progressionAnswer} is wrong answer ;(. Correct answer was ${hidenNum}!`);
+    console.log(`Let's try again, ${userName}`);
+    break;
+  }
+  console.log(`Congratulations, ${userName}!`);
 }
