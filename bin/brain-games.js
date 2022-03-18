d7b9f58ea9f5cdba5f96a3aa16559a3800690eb7
sadyvakasov <1oky.clo@gmail.com>
2022-03-18 12:35:57 +0300

    add brain-calc.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,6 +2,8 @@
 import readlineSync from 'readline-sync';
 import greetings from '../src/cli.js';
 import { questionGenerate } from './brain-even.js';
+import calculateGame from './brain-calc.js';
+import gcd from './brain-gcd.js';
 
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name?');
@@ -20,4 +22,26 @@ for (let i = 0; i < 3; i += 1) {
     break;
   }
   console.log(`Congratulations, ${userName}!`);
+}
+console.log('What is the result of the expression?');
+
+for (let i = 0; i < 3; i += 1) {
+  const calculatingProcess = calculateGame();
+  console.log(`Question: ${calculatingProcess[0]}`);
+  const calcAnswer = readlineSync.question('Your answer:');
+  if (calcAnswer === calculatingProcess[1]) {
+    console.log('Correct!');
+  } else {
+    console.log(`${calcAnswer} is wrong answer ;(. Correct answer was ${calculatingProcess[1]}!`);
+    console.log(`Let's try again, ${userName}`);
+    break;
+  }
+  console.log(`Congratulations, ${userName}!`);
+}
+
+console.log('Find the greatest common divisor of given numbers.');
+for (let i = 0; i < 3; i += 1) {
+  const greatestComDiv = gcd();
+  const twoNumbers = [greatestComDiv[0], greatestComDiv[1]];
+  console.log(`Question: ${twoNumbers}`);
 }
